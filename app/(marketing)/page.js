b38bca4cc0299b2498ed1e90eeb6fd619ba06200
luttfimohamad <luttfimohamad@gmail.com>
2025-05-23 14:34:40 +0700

    create route groups to group folder content and marketing/landing pages

--- app/(marketing)/page.js
+++ app/(marketing)/page.js
@@ -1,4 +1,4 @@
-import logo from '@/assets/logo.jpg'
+import logo from '@/assets/logo.jpg';
 import Link from 'next/link';
 
 export default function HomePage() {
@@ -7,12 +7,12 @@ export default function HomePage() {
       <img src={logo.src} alt="A newspaper" />
       <h1>A News Site For The Next Generation</h1>
       <p>
-        Next News is here to deliver you all the latest news - concise &
+        UnfoldNews is here to deliver you all the latest news - concise &
         unbiased!
       </p>
 
       <p>
-        NextNews aims to provide you with the latest news in a concise and
+        UnfoldNews aims to provide you with the latest news in a concise and
         unbiased manner. We strive to deliver the news in a way that is easy to
         understand and to the point. We want to keep you informed without
         overwhelming you with unnecessary information.
