b1b0f374b6e97bf4ef30056fb2f1e485ebaa616a
luttfimohamad <luttfimohamad@gmail.com>
2025-05-23 14:36:06 +0700

    Fix wording in homepage description for clarity and consistency

--- app/(marketing)/page.js
+++ app/(marketing)/page.js
@@ -12,8 +12,8 @@ export default function HomePage() {
       </p>
 
       <p>
-        .. aims to provide you with the latest news in a concise and unbiased
-        manner. We strive to deliver the news in a way that is easy to
+        UnfoldNews aims to provide you with the latest news in a concise and
+        unbiased manner. We strive to deliver the news in a way that is easy to
         understand and to the point. We want to keep you informed without
         overwhelming you with unnecessary information.
       </p>
